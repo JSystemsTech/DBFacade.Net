6da5b2d04ba04d4a2c3a92dd9c6676d114813e54
jon mcguire <jsmc123@hotmail.com>
2020-09-06 19:46:13 -0400

    Add initial Documentation Sample Code

---
+++ DocumentationExamples/DomainLayer/IMyDomainFacade.cs
@@ -0,0 +1,18 @@
+﻿using DBFacade.DataLayer.Models;
+using DocumentationExamples.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationExamples.DomainLayer
+{
+    public interface IMyDomainFacade
+    {
+        IDbResponse<MyDataModel> GetData();
+        IDbResponse<MyDataModel> GetDataWithOutputParameters();
+        IDbResponse<MyDataModel> GetDataWithParameters(string myString, int myInt);
+        IDbResponse MyTransaction(string myString);
+    }
+}
---
+++ DocumentationExamples/DomainLayer/MyDomainFacade.cs
@@ -0,0 +1,93 @@
+﻿using DBFacade.DataLayer.Models;
+using DBFacade.Exceptions;
+using DocumentationExamples.Models.Data;
+using System;
+
+namespace DocumentationExamples.DomainLayer
+{
+    internal partial class MyDomainFacade : MyDomainFacadeBase, IMyDomainFacade
+    {
+        public MyDomainFacade(string connectionString, string connectionProvider) : base(connectionString, connectionProvider) { }
+       
+         /*Define Calls */
+
+        public IDbResponse<MyDataModel> GetData()
+        {
+            try
+            {
+                return GetDataHandler.Execute();
+            }
+            catch(SQLExecutionException e)
+            {
+                /* handle Sql errors here */
+                return null;
+            }
+            catch (Exception e)
+            {
+                /* handle all other errors here */
+                return null;
+            }
+        }
+        public IDbResponse<MyDataModel> GetDataWithOutputParameters()
+        {
+            try
+            {
+                return GetDataWithOutputParametersHandler.Execute();
+            }
+            catch (SQLExecutionException e)
+            {
+                /* handle Sql errors here */
+                return null;
+            }
+            catch (Exception e)
+            {
+                /* handle all other errors here */
+                return null;
+            }
+        }
+        public IDbResponse<MyDataModel> GetDataWithParameters(string myString, int myInt)
+        {
+            try
+            {
+                return GetDataWithParametersHandler.Execute(myString, myInt);
+            }
+            catch (ValidationException<DbParamsModel<string, int>> e)
+            {
+                /* handle validation errors here */
+                return null;
+            }
+            catch (SQLExecutionException e)
+            {
+                /* handle Sql errors here */
+                return null;
+            }
+            catch (Exception e)
+            {
+                /* handle all other errors here */
+                return null;
+            }
+        }
+        public IDbResponse MyTransaction(string myString)
+        {
+            try
+            {
+                return MyTransactionHandler.Execute(myString);
+            }
+            catch (ValidationException<DbParamsModel<string>> e)
+            {
+                /* handle validation errors here */
+                return null;
+            }
+            catch (SQLExecutionException e)
+            {
+                /* handle Sql errors here */
+                return null;
+            }
+            catch (Exception e)
+            {
+                /* handle all other errors here */
+                return null;
+            }
+        }
+    }
+}
